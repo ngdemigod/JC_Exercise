cfe40758a808072a42a9b3148dbe9f3a85873812
augustinesaleh@gmail.com <augustinesaleh@gmail.com>
2021-08-24 16:18:19 -0400

    initial push test of jc.py to github

---
+++ jc.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template, jsonify
+import requests 
+
+
+#Global Variables
+testURL = "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"

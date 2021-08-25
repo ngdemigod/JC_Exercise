42cfcd35d21ee1f2afc1c8a9c4e2995fc86f9b3d
augustinesaleh@gmail.com <augustinesaleh@gmail.com>
2021-08-24 21:32:34 -0400

    added try-except to functions

--- jc.py
+++ jc.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import flask
-import requests, os
+import os, requests
 
 
 
 #Global Variables
 dir = os.getcwd()
 dFolder = dir + "/downloads/sample.txt"
-
 testURL = "https://www.learningcontainer.com/wp-content/uploads/2020/04/sample-text-file.txt"
 
 
@@ -27,40 +26,53 @@ def home():
     return render_template("home.html")
 
 
-
 #Route to /manage_file endpoint
 @app.route('/manage_file', methods=['POST'])
 def mfile():
-    payload = request.get_json()
-
     jres = {}
     content = []
+    
+    try:
+        payload = request.get_json() #loads the api request into the payload variable
+    except:
+        loadError = "JSON request payload failed to load correctly"
+        jres['error'] = loadError 
+        return flask.jsonify(jres), 500
+    
 
+
+# Based on JSON payload, the app will run a function
     if payload["action"] == "download":
-        dFuction()
-        if os.path.isfile(dFolder): jres['success'] = True
-        return flask.jsonify(jres), 200
+        try:
+            dFuction()
+            if os.path.isfile(dFolder): jres['success'] = True
+            return flask.jsonify(jres), 200
+        except:
+            loadError = "Failed to perform download operation"
+            jres['error'] = loadError 
+            return flask.jsonify(jres), 500
 
     elif payload['action'] == 'read':
-        if os.path.isfile(dFolder):
-            rFile = open(dFolder, 'r').read()
-            content.append(rFile)
-            
-        else:
-            dFuction()
-            rFile = open(dFolder, 'r').read()
-            content.append(rFile)
+        try:
+            if os.path.isfile(dFolder):
+                rFile = open(dFolder, 'r').read()
+                content.append(rFile)
+        
+            else:
+                dFuction()
+                rFile = open(dFolder, 'r').read()
+                content.append(rFile)
+
+        except:
+            loadError = "Failed to perform read operation"
+            jres['error'] = loadError 
+            return flask.jsonify(jres), 500
 
         if content: jres['read'] = content
         content=[]
         jres['success'] = True
 
         return flask.jsonify(jres), 200     
-
-  
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
